9948975fbff66d46041fca5579661d7346ad9f4a
Faisal Akhtar <akhtar.faisal707@gmail.com>
2026-09-12 02:02:41 +0530

    Fix: Correct API imports

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { useAuthStore } from '../store/authStore'
-import { api } from '../services/api'
+import api from '../services/api'
 
 export default function Signup() {
   const navigate = useNavigate()
