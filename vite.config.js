09e30f85fbe4b053e768de255aa802b649683a68
Faisal Akhtar <akhtar.faisal707@gmail.com>
2026-09-14 13:31:58 +0530

    Add routing config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    host: true
+    port: 5174,
+    host: true,
+    historyApiFallback: true 
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   }
 })
