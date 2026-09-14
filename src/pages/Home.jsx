7a1fc258b57eec7f40dcda2bd821db597dd855af
Faisal Akhtar <akhtar.faisal707@gmail.com>
2026-09-14 13:09:27 +0530

    add navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ChevronLeft, ChevronRight, ShoppingCart } from 'lucide-react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer' 
 import api from '../services/api'
 
 export default function Home() {
